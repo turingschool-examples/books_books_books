9939eb11ecbe020f57b910b3007eace9473f0150
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:56:24 -0600

    Add add author and book methond

--- test/library_test.rb
+++ test/library_test.rb
@@ -19,4 +19,27 @@ class LibraryTest < Minitest::Test
     assert_equal [], dpl.books
     assert_equal [], dpl.authors
   end
+
+  def test_it_can_add_authors_and_books
+    dpl = Library.new("Denver Public Library") 
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", 
+                                   last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", 
+                             last_name: "Lee"})    
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")  
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853") 
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expected_authors = [charlotte_bronte, harper_lee]
+    expected_books = [jane_eyre, professor, villette, mockingbird]
+
+    assert_equal expected_authors, dpl.authors
+    assert_equal expected_books, dpl.books
+  end
 end
