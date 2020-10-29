4e66b72a96a4471a198eb9dcc6bad1fdda56150a
Robert Heath <robert.b.heathii@gmail.com>
2020-10-29 11:46:18 -0500

    Test and code publication_time_frame and create helper methods

--- lib/library.rb
+++ lib/library.rb
@@ -21,4 +21,29 @@ class Library
       []
     end
   end
+
+  def find_author(search_author)
+    @authors.find do |author|
+      author == search_author
+    end
+  end
+
+  def last_book(search_author)
+    find_author(search_author).books.max_by do |book|
+      book.publication_year.to_i
+    end
+  end
+
+  def first_book(search_author)
+    find_author(search_author).books.min_by do |book|
+      book.publication_year.to_i
+    end
+  end
+
+  def publication_time_frame_for(search_author)
+    time_frame_hash = {}
+    time_frame_hash[:start] = first_book(search_author).publication_year
+    time_frame_hash[:end] = last_book(search_author).publication_year
+    time_frame_hash
+  end
 end
