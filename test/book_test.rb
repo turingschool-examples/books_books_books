3f1cdfea9f31bad6a629de1c88a646f3b8f0a7e3
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:12:34 -0600

    Fix test assertion typo

--- test/book_test.rb
+++ test/book_test.rb
@@ -7,6 +7,6 @@ class BookTest < Minitest::Test
     @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
   end
   def test_it_exists	
-    assert_instance of Book, @book 
+    assert_instance_of Book, @book 
   end
 end
