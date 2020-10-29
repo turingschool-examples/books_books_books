455976a2506583ad2d7699f6916b69d59dfb7fd9
Michael Foy <foymikek@gmail.com>
2020-10-29 09:50:49 -0600

    Add test_it_has_attributes

---
+++ test/library_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/book"
+require "./lib/author"
+require "./lib/library"
+
+class LibraryTest < Minitest::Test
+
+  def test_it_has_attributes
+    dpl = Library.new("Denver Public Library")
+
+    assert_equal "Denver Public Library", dpl.name
+    assert_equal [], dpl.books
+    assert_equal [], dpl.authors
+  end
+end
