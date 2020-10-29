3d4d7f2811cae16d9b6c878c005a5fa944bf3bdc
Grayson Cunningham <gecun289@gmail.com>
2020-10-29 09:16:02 -0600

    Create initial tests for author class

---
+++ test/author_test.rb
@@ -0,0 +1,30 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/author'
+require './lib/book'
+
+class AuthorTest < Minitest::Test
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
+
+  def test_it_exists
+    assert_instance_of Author, @charlotte_bronte
+  end
+
+  def test_it_has_attributes
+    assert_equal "Charlotte Bronte", @charlotte_bronte.name
+    assert_equal [], @charlotte_bronte.books
+  end
+
+  def test_write
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre.title
+
+    villete = charlotte_bronte.write("Villette", "1853")
+
+    assert_equal [jane_eyre, villete], @charlotte_bronte.books
+  end
+end
