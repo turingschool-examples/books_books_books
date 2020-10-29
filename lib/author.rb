77c22881b3d208a9f7a57cb9221bd5c967ce4789
Grayson Myers <graymyers10@gmail.com>
2020-10-29 09:32:31 -0600

    Add write method and create_book helper to Author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,30 @@
+require './lib/book.rb'
+
 class Author
   attr_reader :name, :books
   def initialize(input_hash)
-    @name = input_hash[:first_name] + " " + input_hash[:last_name]
+    @first_name = input_hash[:first_name]
+    @last_name = input_hash[:last_name]
+
+    @name = @first_name + " " + @last_name
     @books = []
   end
+
+  def write(title,publication_date)
+    book = create_book(title,publication_date)
+    @books << book
+    book
+  end
+
+  def create_book(title,publication_date)
+    Book.new({
+        author_first_name: @first_name,
+        author_last_name: @last_name,
+        title: title,
+        publication_date: publication_date
+      }
+    )
+  end
+
+  
 end
