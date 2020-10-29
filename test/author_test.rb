7b3f505f8c8dc423397c62bd89e10982b4204552
Sam Yeo <yeosamuel95@gmail.com>
2020-10-29 09:23:19 -0600

    Add write method test

--- test/author_test.rb
+++ test/author_test.rb
@@ -17,7 +17,15 @@ class Test < MiniTest::Test
     end
 
     def test_author_can_write_a_book
-        skip
+        jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+        assert_equal Book, jane_eyre.class
+        assert_equal "Jane Eyre", jane_eyre.title
+        assert_equal [jane_eyre], @charlotte_bronte.books
+
+        villette = @charlotte_bronte.write("Villette", "1853")
+
+        assert_equal [jane_eyre, villette], @charlotte_bronte.books
     end
 
 end
