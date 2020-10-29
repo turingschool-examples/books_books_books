68ff0d291c5cd91326da4e8fc7e990f53ea51403
Grayson Cunningham <gecun289@gmail.com>
2020-10-29 09:33:17 -0600

    Create initial tests for library class

---
+++ test/library_test.rb
@@ -0,0 +1,42 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/library'
+require './lib/author'
+
+class LibraryTest < Minitest::Test
+  def setup
+    @dpl = Library.new("Denver Public Library")
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @professor = @charlotte_bronte.write("The Professor", "1857")
+    @villete = @charlotte_bronte.write("Villette", "1853")
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    @mockingbird = mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+  end
+
+  def test_it_exists
+    assert_instance_of Library, @dpl
+  end
+
+  def test_it_has_attributes
+    assert_equal "Denver Public Library", @dpl.name
+    assert_equal [], @dpl.books
+    assert_equal [], @dpl.authors
+  end
+
+  def test_add_author
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    assert_equal [@charlotte_bronte, @harper_lee], @dpl.authors
+    assert_equal [@jane_eyre, @professor, @villete, @mockingbird], @dpl.books
+  end
+
+  def test_publication_time_frame
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    assert_equal ({:start=>"1847", :end=>"1857"}), @dpl.publication_time_frame_for(@charlotte_bronte)
+    assert_equal ({:start=>"1960", :end=>"1960"}), @dpl.publication_time_frame_for(@harper_lee)
+  end
+end
