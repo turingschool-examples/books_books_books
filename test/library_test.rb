f7e1e2efd88c66653099d901053f91e723484abb
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 10:23:38 -0600

    Add checkout test

--- test/library_test.rb
+++ test/library_test.rb
@@ -72,4 +72,24 @@ class LibraryTest < Minitest::Test
     assert_equal expected_bronte, dpl.publication_time_frame_for(charlotte_bronte)
     assert_equal expected_lee, dpl.publication_time_frame_for(harper_lee)
   end
+
+  def test_it_can_checkout_books
+    dpl = Library.new("Denver Public Library") 
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"}) 
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853") 
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    assert_equal false, dpl.checkout(mockingbird)
+    assert_equal false, dpl.checkout(jane_eyre)
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee) 
+
+    assert_equal true, dpl.checkout(mockingbird)
+    assert_equal true, dpl.checkout(jane_eyre)
+  end
 end
