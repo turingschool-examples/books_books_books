d5116b410158c771606a670f0adb360c3b7e744d
Max Ribbans <ribbans.max@gmail.com>
2020-10-29 11:31:35 -0400

    Add write method

--- lib/author.rb
+++ lib/author.rb
@@ -13,4 +13,15 @@ class Author
   def name
     @first_name + " " + @last_name
   end
+
+  def write(title, publication_date)
+    book_deets = {}
+    book_deets[:title] = title
+    book_deets[:publication_date] = publication_date
+    book_deets[:author_first_name] = @first_name
+    book_deets[:author_last_name] = @last_name
+    book = Book.new(book_deets)
+    books << book
+    book
+  end
 end
