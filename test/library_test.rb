cb3837fd1eeadfba3f46ec9284ff84bd8343a347
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 10:01:06 -0600

    Test publication timeframe for authors

--- test/library_test.rb
+++ test/library_test.rb
@@ -34,4 +34,20 @@ class LibraryTest < Minitest::Test
     assert_equal [charlotte_bronte, harper_lee], @dpl.authors
     assert_equal [jane_eyre, professor, villette, mockingbird], @dpl.books
   end
+
+  def test_publication_time_frame_for
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    bronte_time_frame = {:start=>"1847", :end=>"1857"}
+    lee_time_frame =  {:start=>"1960", :end=>"1960"}
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+
+    assert_equal bronte_time_frame, @dpl.publication_time_frame_for(charlotte_bronte)
+    assert_equal lee_time_frame, @dpl.publication_time_frame_for(harper_lee)
+  end
 end
