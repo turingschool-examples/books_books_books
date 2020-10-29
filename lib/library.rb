64a411a31a634a2f3199c67543ae3eb4837fd4de
Sam Yeo <yeosamuel95@gmail.com>
2020-10-29 10:14:26 -0600

    Add publication time frame method

--- lib/library.rb
+++ lib/library.rb
@@ -6,5 +6,27 @@ class Library
         @books = []
         @authors = []
     end
+
+    def add_author(author)
+        @authors << author
+        add_books(author)
+    end
+
+    def add_books(author)
+        author.books.each do |book|
+            @books << book
+        end
+    end
+
+    def publication_time_frame_for(author)
+        date_collection = list_of_dates(author)
+        breakdown = {start: date_collection.first.to_s, end: date_collection.last.to_s}
+    end
+
+    def list_of_dates(author)
+        list_of_dates = author.books.map do |book|
+            book.publication_year.to_i
+        end.sort
+    end
     
 end
