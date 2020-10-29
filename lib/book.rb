aa0875dc2feeb55a8dbf31ab8837625ea21a65be
Grayson Cunningham <gecun289@gmail.com>
2020-10-29 09:09:24 -0600

    Create book class with initialize method and attributes

---
+++ lib/book.rb
@@ -0,0 +1,9 @@
+class Book
+  attr_reader :title, :author, :publication_year
+  
+  def initialize(info)
+    @title = info[:title]
+    @author = "#{info[:author_first_name]} #{info[:author_last_name]}"
+    @publication_year = info[:publication_date].split.last
+  end
+end
