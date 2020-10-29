c253ff23138effff086c3af1cd4527e4ced04223
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:21:59 -0600

    Add attribute methods

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,13 @@
 class Author
+  attr_reader :books
 
   def initialize(data)
     @first_name = data[:first_name]
     @last_name = data[:last_name]
+    @books = []
+  end
+
+  def name
+    @first_name + " " + @last_name
   end
 end
