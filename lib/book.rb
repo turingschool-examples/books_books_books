706536d74734de0e9a6cdb1af59642677d8f8745
Liam Cusack <LCusack8108@gmail.com>
2020-10-29 09:09:34 -0600

    Add book/book_test, first test pass

---
+++ lib/book.rb
@@ -0,0 +1,10 @@
+class Book
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+
+  def initialize(data)
+    @author_first_name = data[:author_first_name]
+    @author_last_name = data[:author_last_name]
+    @title = data[:title]
+    @publication_date = data[:publication_date]
+  end
+end
