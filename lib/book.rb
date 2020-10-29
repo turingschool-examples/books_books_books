3dcb0cfddd49a91b0f7ad716b277bf347ecdbe7f
Yesi Meza <yesi.meza10@gmail.com>
2020-10-29 10:03:46 -0600

    create publication year test and pass

--- lib/book.rb
+++ lib/book.rb
@@ -6,10 +6,14 @@ class Book
     @title = data[:title]
     @author_first_name = data[:author_first_name]
     @author_last_name = data[:author_last_name]
+    @publication_date = data[:publication_date]
   end
 
   def author
     "#{@author_first_name} #{@author_last_name}"
-# require "pry"; binding.pry
+  end
+
+  def publication_year
+    @publication_date[-4,4]
   end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -19,6 +19,15 @@ class BookTest < Minitest::Test
                       title: "To Kill a Mockingbird",
                       publication_date: "July 11, 1960"})
 
-  assert_equal "Harper Lee", book.author
+    assert_equal "Harper Lee", book.author
+  end
+
+  def test_it_knows_publication_year
+    book = Book.new({author_first_name: "Harper",
+                      author_last_name: "Lee",
+                      title: "To Kill a Mockingbird",
+                      publication_date: "July 11, 1960"})
+
+    assert_equal "1960", book.publication_year
   end
 end
