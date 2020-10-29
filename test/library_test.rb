f59a531f3d52905080f97b76cfe4824dfe076799
Max Ribbans <ribbans.max@gmail.com>
2020-10-29 11:38:04 -0400

    Fix typo

--- test/library_test.rb
+++ test/library_test.rb
@@ -8,7 +8,7 @@ class LibraryTest < Minitest::Test
   def test_it_exists_and_has_attributes
     dpl = Library.new("Denver Public Library")
 
-    assert_instace_of Library, dpl
+    assert_instance_of Library, dpl
     assert_equal "Denver Public Library", dpl.name
     assert_equal [], dpl.books
     assert_equal [], dpl.authors
