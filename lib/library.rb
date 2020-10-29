4ebcf51536438eef99a275f9b044a74ef429b30f
Ben Fulton <benjamin.fulton99@gmail.com>
2020-10-29 10:09:01 -0600

    Add author to library along with respective books

--- lib/library.rb
+++ lib/library.rb
@@ -1,6 +1,24 @@
 class Library
-    attr_reader
-    def initialize(name)
+    attr_reader :name,
+                :books,
+                :authors
+    def initialize(name, books = [] , authors = [])
         @name = name
+        @books = books
+        @authors = authors
     end
+    
+    def add_author(author)
+        @authors << author
+        @authors.map do |author|
+            author.books.each do |book|
+                @books << book
+            end
+        end
+        @authors
+    end
+
+    
+
+
 end
