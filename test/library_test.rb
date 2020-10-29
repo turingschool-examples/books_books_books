59454d5ad3638ccb3140f7bc956063758335c200
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 10:26:49 -0600

    Test it can return books

--- test/library_test.rb
+++ test/library_test.rb
@@ -74,4 +74,25 @@ class LibraryTest < Minitest::Test
 
     assert_equal [jane_eyre, villette], @dpl.checked_out_books
   end
+
+  def test_it_can_return_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+    
+    @dpl.checkout(jane_eyre)
+    @dpl.checkout(villette)
+    @dpl.checkout(mockingbird)
+
+    @dpl.return(mockingbird)
+
+    assert_equal [jane_eyre, villette], @dpl.checked_out_books
+    assert_equal [professor, mockingbird], @dpl.books
+  end
 end
