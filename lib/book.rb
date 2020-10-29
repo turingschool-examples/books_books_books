bd91ca308015f63abc7b6e8c4e67f231bf956ced
Catherine Dean <catherinemdean15@gmail.com>
2020-10-29 09:13:59 -0600

    add book test and Book class

---
+++ lib/book.rb
@@ -0,0 +1,19 @@
+class Book
+  attr_reader :title
+
+  def initialize(info)
+    @author_first_name = info[:author_first_name]
+    @author_last_name = info[:author_last_name]
+    @title = info[:title]
+    @publication_date = info[:publication_date]
+  end
+
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def year
+    @publication_date.split(" ")[2] 
+  end
+
+end
