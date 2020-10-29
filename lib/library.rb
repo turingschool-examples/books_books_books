5b1cd400f97a62bd221d2ce0f9be24f2b84a18f0
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 10:08:42 -0600

    Add add author method

--- lib/library.rb
+++ lib/library.rb
@@ -8,4 +8,15 @@ class Library
     @books = []
     @authors = []
   end
+
+  def add_author(author)
+    @authors << author
+    add_author_books(author)
+  end
+
+  def add_author_books(author)
+    author.books.each do |book|
+      @books << book
+    end
+  end
 end
