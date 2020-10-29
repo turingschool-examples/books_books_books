365500be5c4ae268ed64936008080851423869dc
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:46:38 -0600

    Test library has attributes

--- test/library_test.rb
+++ test/library_test.rb
@@ -10,4 +10,10 @@ class LibraryTest < Minitest::Test
   def test_it_exists	
     assert_instance_of Library, @dpl 
   end
+
+  def test_it_has_attributes
+    assert_equal "Denver Public Library", @dpl.name
+    assert_equal [], @dpl.books
+    assert_equal [], @dpl.authors
+  end
 end
