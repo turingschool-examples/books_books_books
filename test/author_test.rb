ee613b1fe32cb30e7e46c14a464fc66e7f2d1d92
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:22:56 -0600

    Test it has attributes

--- test/author_test.rb
+++ test/author_test.rb
@@ -6,8 +6,13 @@ class AuthorTest < Minitest::Test
   def setup
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
   end
-  
+
   def test_it_exists	
     assert_instance_of Author, @charlotte_bronte 
+  end
+
+  def test_it_has_attributes
+    assert_equal "Charlotte Bronte",@charlotte_bronte.name
+    assert_equal [], @charlotte_bronte.books
   end
 end
