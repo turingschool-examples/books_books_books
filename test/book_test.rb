5fe05ef790c3389695d4e952af172c9ead15946c
Joseph Budina <joebud3@gmail.com>
2020-10-29 09:10:05 -0600

    Add test it_exists_with_attributes

---
+++ test/book_test.rb
@@ -0,0 +1,16 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/book'
+
+class BookTest < Minitest::Test
+  def setup
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
+  def test_it_exists_with_attributes
+    assert_instance_of Book, @book 
+    assert_equal "To Kill a Mockingbird", @book.title
+    assert_equal "Harper Lee", @book.author 
+    assert_equal "1960", @book.publication_year
+  end
+end
