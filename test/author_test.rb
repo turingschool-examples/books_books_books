e7173be1113f08e862a85f4d102e69803e8e0bee
Phil McCarthy <philmcc2631@gmail.com>
2020-10-29 09:17:56 -0600

    Add author initialization test

---
+++ test/author_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/author'
+require './lib/book'
+class AuthorTest < Minitest::Test
+  def setup
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Author, charlotte_bronte
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+    assert_equal [], charlotte_bronte.books
+  end
+end
