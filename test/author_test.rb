2c53179fa43eb16eccf95b6dad7a289fe053a262
Max Ribbans <ribbans.max@gmail.com>
2020-10-29 11:19:45 -0400

    Add test it exists and has attributes

--- test/author_test.rb
+++ test/author_test.rb
@@ -4,5 +4,9 @@ require './lib/book'
 require './lib/author'
 
 class AuthorTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
+    assert_equal [], charlotte_bronte.books
+  end
 end
