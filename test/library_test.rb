561e2256bf14bc1cdfe992e38ca3dabb131a664d
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:48:09 -0600

    Add attribute test

--- test/library_test.rb
+++ test/library_test.rb
@@ -11,4 +11,12 @@ class LibraryTest < Minitest::Test
 
     assert_instance_of Library, dpl
   end
+
+  def test_it_has_attributes
+    dpl = Library.new("Denver Public Library") 
+
+    assert_equal "Denver Public Library", dpl.name
+    assert_equal [], dpl.books
+    assert_equal [], dpl.authors
+  end
 end
