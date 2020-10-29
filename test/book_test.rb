ae10f85cb6923ff59e91281e3214099ba656f434
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:12:08 -0600

    Add attribute test

--- test/book_test.rb
+++ test/book_test.rb
@@ -12,4 +12,15 @@ class BookTest < Minitest::Test
 
   assert_instance_of Book, book                    
   end
+
+  def test_it_has_attributes
+    book = Book.new({author_first_name: "Harper", 
+      author_last_name: "Lee", 
+      title: "To Kill a Mockingbird", 
+      publication_date: "July 11, 1960"})
+
+      assert_equal "To Kill a Mockingbird", book.title
+      assert_equal "Harper Lee", book.author
+      assert_equal "1960", book.publication_year
+  end
 end
