8358d57cc9959c2c12c95a824974a14c7aa91518
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:05:49 -0600

    Set up book test and test it exists

---
+++ test/book_test.rb
@@ -0,0 +1,12 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/book'
+
+class BookTest < Minitest::Test
+  def setup
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+  def test_it_exists	
+    assert_instance of Book, @book 
+  end
+end
