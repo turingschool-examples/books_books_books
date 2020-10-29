70851602c374f7abe9040eaf0a87c5836de2dbed
Max Ribbans <ribbans.max@gmail.com>
2020-10-29 11:47:12 -0400

    Add test it can check publication time frame for author

--- test/library_test.rb
+++ test/library_test.rb
@@ -28,4 +28,22 @@ class LibraryTest < Minitest::Test
     assert_equal [charlotte_bronte, harper_lee], dpl.authors
     assert_equal 4, dpl.books.length
   end
+
+  def test_it_can_check_publication_time_fram_for
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expected = {:start=>"1847", :end=>"1857"}
+    assert_equal expected, dpl.publication_time_frame_for(charlotte_bronte)
+
+    expected = {:start=>"1960", :end=>"1960"}
+    assert_equal expected, dpl.publication_time_frame_for(harper_lee)
+  end
 end
