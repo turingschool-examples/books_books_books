03c0dcc15ef1a111aa6d9a80e16b80f25157ca8e
Grayson Cunningham <gecun289@gmail.com>
2020-10-29 10:06:32 -0600

    Add tests for checkout and return methods of library class

--- test/library_test.rb
+++ test/library_test.rb
@@ -11,7 +11,7 @@ class LibraryTest < Minitest::Test
     @professor = @charlotte_bronte.write("The Professor", "1857")
     @villete = @charlotte_bronte.write("Villette", "1853")
     @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-    @mockingbird = mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   def test_it_exists
@@ -46,5 +46,54 @@ class LibraryTest < Minitest::Test
 
   def test_last_book
     assert_equal @professor, @dpl.last_book(@charlotte_bronte)
+  end
+
+  def test_checkout
+    assert_equal false, @dpl.checkout(@mockingbird)
+    assert_equal false, @dpl.checkout(@jane_eyre)
+
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    assert_equal true, @dpl.checkout(@jane_eyre)
+  end
+
+  def test_checked_out_books
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    assert_equal [], @dpl.checked_out_books
+
+    @dpl.checkout(@jane_eyre)
+
+    assert_equal [@jane_eyre], @dpl.checked_out_books
+    assert_equal false, @dpl.checkout(@jane_eyre)
+  end
+
+  def test_return
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+    @dpl.checkout(@jane_eyre)
+
+    @dpl.return(@jane_eyre)
+    assert_equal [], @dpl.checked_out_books
+    assert_equal true, @dpl.checkout(@jane_eyre)
+  end
+
+  def test_most_popular_book
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    @dpl.checkout(@jane_eyre)
+    @dpl.return(@jane_eyre)
+    @dpl.checkout(@jane_eyre)
+    @dpl.checkout(@villete)
+    @dpl.checkout(@mockingbird)
+    @dpl.return(@mockingbird)
+    @dpl.checkout(@mockingbird)
+    @dpl.return(@mockingbird)
+    @dpl.checkout(@mockingbird)
+
+    assert_equal @mockingbird, @dpl.most_popular_book
   end
 end
