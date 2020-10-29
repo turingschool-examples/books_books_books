7954eca244a6ab047cd608f926e27f8635f48c36
Ely Hess <ely.hess@me.com>
2020-10-29 09:08:49 -0600

    Reformat

--- test/book_test.rb
+++ test/book_test.rb
@@ -9,7 +9,7 @@ class BookTest < Minitest::Test
 
     assert_instance_of Book, book
     assert_equal "To Kill a Mockingbird", book.title
-    assert_equal "Harper Lee", book.title
+    assert_equal "Harper Lee", book.author
     assert_equal "1960", book.publication_year
   end
 
