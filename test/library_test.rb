08741a2b3e6c1460ce7cbad9627f20bb34d0ac82
Max Ribbans <ribbans.max@gmail.com>
2020-10-29 11:35:29 -0400

    Test it exists and has attributes

--- test/library_test.rb
+++ test/library_test.rb
@@ -3,3 +3,14 @@ require 'minitest/pride'
 require './lib/book'
 require './lib/author'
 require './lib/library'
+
+class LibraryTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    dpl = Library.new("Denver Public Library")
+
+    assert_instace_of Library, dpl
+    assert_equal "Denver Public Library", dpl.name
+    assert_equal [], dpl.books
+    assert_equal [], dpl.authors
+  end
+end
