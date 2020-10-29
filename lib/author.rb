a314cbc0b80e7daedd3ec598bcc296659ce4a7a3
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:39:02 -0600

    Add write test to author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,18 @@
 class Author
   attr_reader :name,
               :books
-              
+
   def initialize(data)
     @name = data[:first_name] + " " + data[:last_name]
     @books = []
+  end
+
+  def write(title, publication_date)
+    author_name = self.name.split(" ")
+    author_first_name = author_name[0]
+    author_last_name = author_name[1]
+    book = Book.new({title: title, publication_date: publication_date, author_first_name: author_first_name, author_last_name: author_last_name})
+    @books << book
+    book
   end
 end
