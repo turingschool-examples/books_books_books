3a0f28717ea63d72470ffe07134bc430dae518de
Grayson Myers <graymyers10@gmail.com>
2020-10-29 09:42:00 -0600

    Add test for add_author method of Library class

--- test/library_test.rb
+++ test/library_test.rb
@@ -1,10 +1,19 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/library.rb"
+require "./lib/author.rb"
 
 class Test < Minitest::Test
   def setup
     @library = Library.new("Denver Public Library")
+
+    @author1 = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @book1 = @author1.write("Jane Eyre", "October 16, 1847")
+    @book2 = charlotte_bronte.write("The Professor", "1857")
+    @book3 = charlotte_bronte.write("Villette", "1853")
+
+    @author2 = Author.new({first_name: "Harper", last_name: "Lee"})
+    @book4 = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   def test_it_exists_and_has_attributes
@@ -14,5 +23,11 @@ class Test < Minitest::Test
     assert_equal [], @library.authors
   end
 
-
+  def test_it_adds_authors
+    assert_equal [], @library.authors
+    @library.add_author(@author1)
+    @library.add_author(@author2)
+    assert_equal [@author1, @author2], @library.authors
+    assert_equal [@book1, @book2, @book3, @book4], @library.books
+  end
 end
