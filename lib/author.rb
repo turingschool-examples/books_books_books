a7225a52bba77de05cc1db0319e27eddde629363
Caryn Keffer <carynkeffer@gmail.com>
2020-10-29 12:08:06 -0400

    add author class and test

---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author
+  attr_reader :first_name,
+              :last_name
+
+  def initialize(
+    first_name:,
+    last_name:
+  )
+
+    @first_name = first_name
+    @last_name = last_name
+  end
+end
