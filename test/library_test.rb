eb99b889b303c03efb3ae4c14804e936cf309c20
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:53:02 -0600

    Test library can add authors and that books aby author are added automatically

--- test/library_test.rb
+++ test/library_test.rb
@@ -1,6 +1,9 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/library'
+require './lib/author'
+require './lib/book'
+
 
 class LibraryTest < Minitest::Test
   def setup
@@ -15,5 +18,20 @@ class LibraryTest < Minitest::Test
     assert_equal "Denver Public Library", @dpl.name
     assert_equal [], @dpl.books
     assert_equal [], @dpl.authors
+  end
+
+  def test_it_can_add_authors_and_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee) 
+
+    assert_equal [charlotte_bronte, harper_lee], @dps.authors
+    assert_equal [jane_eyre, professor, villette, mockingbird], @dps.books
   end
 end
