eb669717ca8dadc7ea8d076925755f95734021fb
Liam Cusack <LCusack8108@gmail.com>
2020-10-29 09:15:23 -0600

    Add completed iteration 1

--- test/book_test.rb
+++ test/book_test.rb
@@ -9,8 +9,16 @@ class BookTest < Minitest::Test
       title: "To Kill a Mockingbird",
       publication_date: "July 11, 1960"})
   end
+
   def test_it_exists
 
     assert_instance_of Book, @book
+  end
+
+  def test_attributes
+
+    assert_equal "To Kill a Mockingbird", @book.title
+    assert_equal "Harper Lee", @book.author
+    assert_equal "1960", @book.publication_year
   end
 end
