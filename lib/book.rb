3cb5a42152ce69b90ac769bff878cc19b01419a5
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 10:55:18 -0600

    Refactor book class initialize by removing uunused attribute

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,11 @@
 class Book
-  attr_reader :publication_date, 
-              :title,
+  attr_reader :title,
               :author,
               :publication_year
 
   def initialize(data)
     @title = data[:title]
-    @publication_date = data[:publication_date]
     @author = data[:author_first_name] + " " + data[:author_last_name]
-    @publication_year = @publication_date[-4..-1]
+    @publication_year = data[:publication_date][-4..-1]
   end
 end
