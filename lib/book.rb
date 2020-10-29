dc6d441b64b30fcd05b568ae5f76e1b86ad15d59
Michael Foy <foymikek@gmail.com>
2020-10-29 10:21:06 -0600

    Add book attributes

--- lib/book.rb
+++ lib/book.rb
@@ -5,6 +5,8 @@ class Book
     @title = data[:title]
     @author = data[:author_first_name] + " " + data[:author_last_name]
     @publication_date = data[:publication_date]
+    @rented = nil
+    @issue_count = 0
   end
 
   def publication_year
