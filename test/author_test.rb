c396da03e64879eed12b9f48620159ec053e8310
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:26:44 -0600

    Test author can write books

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,7 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/author'
+require './lib/book'
 
 class AuthorTest < Minitest::Test
   def setup
@@ -14,5 +15,12 @@ class AuthorTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Charlotte Bronte",@charlotte_bronte.name
     assert_equal [], @charlotte_bronte.books
+  end
+
+  def test_author_can_write_books
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre.title
   end
 end
