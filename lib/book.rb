cc213d70fca5955aeb6ca78f640238bf287a029c
Dominic Padula <domopadula2192@gmail.com>
2020-10-29 09:16:25 -0600

    Add class attributes to pass test

---
+++ lib/book.rb
@@ -0,0 +1,9 @@
+class Book
+  attr_reader:author_first_name,:author_last_name, :title, :publication_date
+  def initialize(details)
+    @author_first_name = details[:author_first_name]
+    @author_last_name = details[:author_last_name]
+    @title = details[:title]
+    @publication_date = details[:publication_date]
+  end
+end
