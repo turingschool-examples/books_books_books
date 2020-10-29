8b2a073fad13c7cb1f2a4deca5819826980ca790
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 10:11:18 -0600

    Add publication time frame test

--- test/library_test.rb
+++ test/library_test.rb
@@ -42,4 +42,34 @@ class LibraryTest < Minitest::Test
     assert_equal expected_authors, dpl.authors
     assert_equal expected_books, dpl.books
   end
+
+  def test_publication_time_frame
+    dpl = Library.new("Denver Public Library") 
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", 
+                                   last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", 
+                             last_name: "Lee"})    
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")  
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853") 
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expected_bronte = {
+      :start=>"1847", 
+      :end=>"1857"
+    }
+
+    expected_lee = {
+      :start=>"1960", 
+      :end=>"1960"
+    }
+
+    assert_equal expected_bronte, dpl.publication_time_frame_for(charlotte_bronte)
+    assert_equal expected_lee, dpl.publication_time_frame_for(harper_lee)
+  end
 end
