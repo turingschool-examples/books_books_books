e52fe30d8188e02e465b6bcabb02ea847f723ffb
Joseph Budina <joebud3@gmail.com>
2020-10-29 09:41:32 -0600

    Initialize Author with attributes, delete text from setup in test

--- test/author_test.rb
+++ test/author_test.rb
@@ -5,8 +5,6 @@ require './lib/author'
 
 class AuthorTest < Minitest::Test
   def setup
-    @book = Book.new({author_first_name: ¸"Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
-
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
   end
 
