45b92eccd2b5cd3e63ffab6190658cf05152f2d7
Ben Fulton <benjamin.fulton99@gmail.com>
2020-10-29 09:11:04 -0600

    Add author full name method

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,17 @@
 class Book
-    def initialize
-        
+    attr_reader :author_first_name,
+                :author_last_name,
+                :title,
+                :publication_date
+
+    def initialize(info)
+        @author_first_name = info[:author_first_name]
+        @author_last_name = info[:author_last_name]
+        @title = info[:title]
+        @publication_date = info[:publication_date]
+    end
+
+    def author
+        @author_first_name + " " + @author_last_name
     end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -9,4 +9,10 @@ class BookTest < Minitest::Test
     def test_it_exists
         assert_instance_of Book , @book
     end
+
+    def test_it_has_attributes
+       assert_equal "To Kill a Mockingbird",  @book.title
+       assert_equal "Harper Lee" , @book.author
+       assert_equal "1960" , @book.publication_year
+    end
 end
