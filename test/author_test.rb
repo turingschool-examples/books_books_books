f08bb7f25865abd08975e77b611cceacb781832e
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:21:10 -0600

    Add attribute test

--- test/author_test.rb
+++ test/author_test.rb
@@ -10,4 +10,12 @@ class AuthorTest < Minitest::Test
                                    last_name: "Bronte"})
     asser_instance_of Author, charlotte_bronte
   end
+
+  def test_it_has_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", 
+                                   last_name: "Bronte"})
+
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+    assert_equal [], charlotte_bronte.books
+  end
 end
