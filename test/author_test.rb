f4f96b8e2e0da8d090790a6cc523cd94a5f8be6e
Grayson Myers <graymyers10@gmail.com>
2020-10-29 09:22:03 -0600

    Add test for write method of Author class

--- test/author_test.rb
+++ test/author_test.rb
@@ -13,4 +13,17 @@ class AuthorTest < Minitest::Test
     assert_equal "Charlotte Bronte", @author.name
     assert_equal [], @author.books
   end
+
+  def test_it_can_write_books
+    assert_equal [], @author.books
+    book1 = @author.write("Jane Eyre", "October 16, 1847")
+    assert_equal [book1], @author.books
+    book2 = charlotte_bronte.write("Villette", "1853")
+    assert_equal [book1, book2], @author.books
+
+    assert_instance_of Book, book1
+    assert_equal "Jane Eyre", book1.title
+    assert_equal "1847", book1.title
+    assert_equal "Charlotte Bronte", book1.author
+  end
 end
