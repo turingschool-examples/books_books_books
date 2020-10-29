2ed2148bebcfcbbe91231798701a90710fabd5f4
Dominic Padula <domopadula2192@gmail.com>
2020-10-29 09:35:36 -0600

    Add passing test for elements in hash for Author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,8 @@
 class Author
+  attr_reader:first_name,:last_name,:books
   def initialize(details)
-  end 
+    @first_name = details[:first_name]
+    @last_name = details[:last_name]
+    @books = []
+  end
 end
