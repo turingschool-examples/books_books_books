506978c05dcece0d8580f65d80603714831d97f4
Max Ribbans <ribbans.max@gmail.com>
2020-10-29 11:26:37 -0400

    Test it can write

--- test/author_test.rb
+++ test/author_test.rb
@@ -15,4 +15,16 @@ class AuthorTest < Minitest::Test
 
     assert_equal "Charlotte Bronte", charlotte_bronte.name
   end
+
+  def test_it_can_write
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre.title
+
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    assert_equal author.books, [jane_eyre, villette]
+  end
 end
