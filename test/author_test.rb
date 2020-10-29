9c65cf54ba9e890a36eeef2b193ac94f58e2a16b
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:27:25 -0600

    Add add book test

--- test/author_test.rb
+++ test/author_test.rb
@@ -8,7 +8,7 @@ class AuthorTest < Minitest::Test
   def test_it_exists
     charlotte_bronte = Author.new({first_name: "Charlotte", 
                                    last_name: "Bronte"})
-    asser_instance_of Author, charlotte_bronte
+    assert_instance_of Author, charlotte_bronte
   end
 
   def test_it_has_attributes
@@ -17,5 +17,21 @@ class AuthorTest < Minitest::Test
 
     assert_equal "Charlotte Bronte", charlotte_bronte.name
     assert_equal [], charlotte_bronte.books
+  end
+
+  def test_it_can_add_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", 
+                                   last_name: "Bronte"})
+    
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")                             
+
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
+
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    expected = [jane_eyre, villette]
+
+    assert_equal expected, charlotte_bronte.books
   end
 end
