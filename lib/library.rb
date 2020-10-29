67465c00a87340f6a768d7b206bbd07af15c4e1c
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 10:09:27 -0600

    Add publication_time_frame_for method to librrary class

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,16 @@ class Library
       books << book
     end
   end
+
+  def publication_time_frame_for(author)
+    max = 0
+    min = 10_000
+    author.books.each do |book|
+      pub_year = book.publication_year.to_i
+      max = pub_year if pub_year > max
+      min = pub_year if pub_year < min
+    end
+
+    {:start => min.to_s, :end => max.to_s}
+  end
 end
