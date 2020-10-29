85053f874b3d4de6924c5dfb4cb12bd6be8ad47c
Michael Foy <foymikek@gmail.com>
2020-10-29 09:19:33 -0600

    Add test_it_has_attributes

---
+++ test/author_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/book"
+require "./lib/author"
+
+class AuthorTest < Minitest::Test
+
+  def test_it_has_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+    assert_equal [], charlotte_bronte.books
+  end
+
+end
