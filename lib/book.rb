06a8e76ae48837af69f0c8ada96ef72bd6c3c418
Aiden Mendez <aidenmendez@gmail.com>
2020-10-29 09:19:12 -0600

    Set up author test and test it exists

--- lib/book.rb
+++ lib/book.rb
@@ -5,13 +5,13 @@ class Book
               :title,
               :author,
               :publication_year
-              
+
   def initialize(data)
     @author_first_name = data[:author_first_name]
     @author_last_name = data[:author_last_name]
     @title = data[:title]
     @publication_date = data[:publication_date]
-    @author = @author_first_name + @author_last_name
+    @author = @author_first_name + " " + @author_last_name
     @publication_year = @publication_date[-4..-1]
   end
 end
