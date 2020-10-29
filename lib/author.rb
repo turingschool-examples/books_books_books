88a50feb745e6593236012b20d3c0bc207befdd9
Robert Heath <robert.b.heathii@gmail.com>
2020-10-29 10:23:30 -0500

    initialize author

---
+++ lib/author.rb
@@ -0,0 +1,10 @@
+require './lib/book'
+
+class Author
+  attr_reader :name, :books
+
+  def initialize(author_data)
+    @name = author_data[:first_name] + " " + author_data[:last_name]
+    @books = []
+  end
+end
--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 class Book
   attr_reader :title, :author, :publication_year
 
-  def initialize(data)
+  def initialize(book_hash)
     @title  = data[:title]
     @author = data[:author_first_name] + " " + data[:author_last_name]
     @publication_year = data[:publication_date].slice(-4, 4)
