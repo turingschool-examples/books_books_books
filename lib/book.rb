d77b556d43409a26f91b9db7c65927385dc423f5
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-29 09:09:18 -0600

    Add Book class

---
+++ lib/book.rb
@@ -0,0 +1,9 @@
+class Book
+
+  def initialize(data)
+    @author_first_name = data[:author_first_name]
+    @author_last_name = data[:author_last_name]
+    @title = data[:title]
+    @publication_date = data[:publication_date]
+  end
+end
