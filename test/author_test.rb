2ff9c9b4a75c9fd0867cd93178e63cfcbf83abe0
Adam Etzion <adametzion90@gmail.com>
2020-10-29 09:40:52 -0600

    Add iteration 2 author class

--- test/author_test.rb
+++ test/author_test.rb
@@ -12,21 +12,21 @@ class AuthorTest < Minitest::Test
     end
 
     def test_it_exists
-        assert_instance_of Auhtor, @charlotte_bronte
+        assert_instance_of Author, @charlotte_bronte
     end
 
     def test_it_has_attributes
-        assert_equal "Charlotte Bronte", @author.name
-        assert_equal [], @author.books
+        assert_equal "Charlotte Bronte", @charlotte_bronte.name
+        assert_equal [], @charlotte_bronte.books
     end
 
     def test_it_can_write_books
-        jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-        villette = charlotte_bronte.write("Villette", "1853")
-
+        jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+        villette = @charlotte_bronte.write("Villette", "1853")
+        require 'pry'; binding.pry
         assert_equal Book, jane_eyre.class
         assert_equal "Jane Eyre", jane_eyre.title
-        assert_equal [jane_eyre, villette], charlotte_bronte.books
+        assert_equal [jane_eyre, villette], @charlotte_bronte.books
     end
 
 end
