a3c46950c571719f7d37b09e269ef512769f55d6
Robert Heath <robert.b.heathii@gmail.com>
2020-10-29 10:16:09 -0500

    Code and test initialization of book

---
+++ lib/book.rb
@@ -0,0 +1,9 @@
+class Book
+  attr_reader :title, :author, :publication_year
+
+  def initialize(data)
+    @title  = data[:title]
+    @author = data[:author_first_name] + " " + data[:author_last_name]
+    @publication_year = data[:publication_date].slice(-4, 4)
+  end
+end
